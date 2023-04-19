dddfadf5e16f82f738cd2d8c0cd66a87636248f1
Slowyn <y231966mdk@gmail.com>
2023-04-20 01:47:15 +0200

    Simple kafka reader

--- backend/kafka-client/src/lib.rs
+++ backend/kafka-client/src/lib.rs
@@ -3,3 +3,4 @@ mod message_producer;
 
 pub use message_consumer::MessageConsumer;
 pub use message_producer::MessageProducer;
+pub use rdkafka;
--- backend/kafka-client/src/message_consumer.rs
+++ backend/kafka-client/src/message_consumer.rs
@@ -1,6 +1,10 @@
-use rdkafka::config::ClientConfig;
-use rdkafka::message::{Header, OwnedHeaders};
-use rdkafka::producer::{FutureProducer, FutureRecord};
-use rdkafka::util::get_rdkafka_version;
+use rdkafka::client::ClientContext;
+use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
+use rdkafka::consumer::stream_consumer::StreamConsumer;
+use rdkafka::consumer::{CommitMode, Consumer, ConsumerContext, Rebalance};
+use rdkafka::error::KafkaResult;
+use rdkafka::message::{Headers, Message};
+use rdkafka::topic_partition_list::TopicPartitionList;
 
+#[derive(Clone)]
 pub struct MessageConsumer {}
